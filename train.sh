ef89f2a3d6400ef1fb3e5a4b7309005fac08c422
Focusjd <jiaodian0102@126.com>
2021-08-15 14:08:48 +0800

    add python args

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ conda env create -f environment.yml
 source activate skillearn
 pip install Pillow
 mkdir readytorun
-python darts-LPT/train_search_ts.py
+python darts-LPT/train_search_ts.py --batch_size 8 --gpu 0,1,2,3,4 is_parallel 1
