3f08f6990db9c3493cc585c8a59c43efc2a8ddc1
FooxKing <55744589+Focusjd@users.noreply.github.com>
2021-08-16 13:00:40 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ conda env create -f environment.yml
 source activate skillearn
 pip install Pillow
 mkdir readytorun
-python darts-LPT/train_search_ts.py --batch_size 6 --gpu 0,1,2,3 is_parallel 1
+python darts-LPT/train_search_ts.py --batch_size 6 --gpu 0,1,2,3 --is_parallel 1
