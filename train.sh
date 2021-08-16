95d0d97be6435b17fe7aaee415afa4dfe9671c34
Focusjd <jiaodian0102@126.com>
2021-08-17 01:11:29 +0800

    modify

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-sleep infinity
 nvidia-smi
 conda env create -f environment.yml
 source activate skillearn
 cd /dianjiao-pvc/LPT-COVIDx/darts-LPT
-python train_search_ts.py --batch_size 6 --layers 6
+python train_search_ts.py --batch_size 5 --layers 8
