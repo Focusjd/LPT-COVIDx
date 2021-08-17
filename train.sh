e91e168d518f946f374c71ee532fc64ea7fc0995
FooxKing <55744589+Focusjd@users.noreply.github.com>
2021-08-18 01:59:38 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-sleep infinity
 screen -S exp
 bash
 nvidia-smi
 conda env create -f environment.yml
 source activate skillearn
 cd /dianjiao-pvc/LPT-COVIDx/darts-LPT
-python train_search_ts.py python train_search_ts.py --layers 8 --batch_size 12 --init_channels 6
+python train_search_ts.py python train_search_ts.py --layers 6 --batch_size 8 --init_channels 6
