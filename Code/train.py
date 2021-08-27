17b836f2f52a2a68c90e99fc98846c8f98792975
Focusjd <jiaodian0102@126.com>
2021-08-28 00:15:43 +0800

    a

--- Code/train.py
+++ Code/train.py
@@ -39,8 +39,12 @@ parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
 
+#darts-30epochs 
+parser.add_argument('--arch', type=str, default="Genotype(normal=[('avg_pool_3x3', 1), ('sep_conv_5x5', 0), ('max_pool_3x3', 0), ('max_pool_3x3', 1), ('max_pool_3x3', 3), ('max_pool_3x3', 0), ('max_pool_3x3', 0), ('sep_conv_3x3', 1)], normal_concat=range(2, 6), reduce=[('skip_connect', 0), ('max_pool_3x3', 1), ('sep_conv_3x3', 1), ('dil_conv_3x3', 2), ('skip_connect', 0), ('skip_connect', 3), ('skip_connect', 4), ('skip_connect', 3)], reduce_concat=range(2, 6))", help='which architecture to use')
+
+
 # darts
-parser.add_argument('--arch', type=str, default="Genotype(normal=[('skip_connect', 0), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 2), ('max_pool_3x3', 0), ('max_pool_3x3', 1), ('sep_conv_5x5', 4), ('avg_pool_3x3', 0)], normal_concat=range(2, 6), reduce=[('skip_connect', 0), ('max_pool_3x3', 1), ('sep_conv_3x3', 1), ('dil_conv_5x5', 0), ('sep_conv_3x3', 3), ('skip_connect', 0), ('sep_conv_3x3', 1), ('max_pool_3x3', 2)], reduce_concat=range(2, 6))", help='which architecture to use')
+# parser.add_argument('--arch', type=str, default="Genotype(normal=[('skip_connect', 0), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 2), ('max_pool_3x3', 0), ('max_pool_3x3', 1), ('sep_conv_5x5', 4), ('avg_pool_3x3', 0)], normal_concat=range(2, 6), reduce=[('skip_connect', 0), ('max_pool_3x3', 1), ('sep_conv_3x3', 1), ('dil_conv_5x5', 0), ('sep_conv_3x3', 3), ('skip_connect', 0), ('sep_conv_3x3', 1), ('max_pool_3x3', 2)], reduce_concat=range(2, 6))", help='which architecture to use')
 
 # lpt
 # parser.add_argument('--arch', type=str, default="Genotype(normal=[('max_pool_3x3', 1), ('max_pool_3x3', 0), ('sep_conv_5x5', 2), ('sep_conv_5x5', 1), ('sep_conv_3x3', 2), ('dil_conv_3x3', 3), ('dil_conv_3x3', 4), ('dil_conv_5x5', 2)], normal_concat=range(2, 6), reduce=[('sep_conv_5x5', 0), ('dil_conv_3x3', 1), ('skip_connect', 0), ('dil_conv_5x5', 2), ('max_pool_3x3', 3), ('sep_conv_3x3', 0), ('max_pool_3x3', 3), ('max_pool_3x3', 4)], reduce_concat=range(2, 6))", help='which architecture to use')
