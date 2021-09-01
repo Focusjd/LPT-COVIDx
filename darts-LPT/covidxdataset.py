3589dd5fadf8502006d989536b2a66a94c3f74e9
FooxKing <55744589+Focusjd@users.noreply.github.com>
2021-09-01 16:39:42 +0800

    Update covidxdataset.py

--- darts-LPT/covidxdataset.py
+++ darts-LPT/covidxdataset.py
@@ -63,7 +63,7 @@ class COVIDxDataset(Dataset):
                 transforms.Resize(256),
                 transforms.RandomResizedCrop((224), scale=(0.5, 1.0)),
                 transforms.RandomHorizontalFlip(),
-                transforms.RandomAffine(degrees=(-10, 10), translate=(0.1, 0.1)),
+#                 transforms.RandomAffine(degrees=(-10, 10), translate=(0.1, 0.1)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
